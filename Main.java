36a417abb0bc35a71b49dc8e4b949084dcfacfa4
Y2K-BR4YAN <143621993+Y2K-BR4YAN@users.noreply.github.com>
2023-09-30 21:13:46 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,46 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 import java.util.Scanner;
 
-public class ParImpar {
+public class CajeroAutomatico {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Por favor, ingrese un número entero: ");
-        int numero = scanner.nextInt();
-        
-        // Verificar si el número es par o impar
-        if (numero % 2 == 0) {
-            System.out.println("El número " + numero + " es PAR.");
-        } else {
-            System.out.println("El número " + numero + " es IMPAR.");
+
+        double saldo = 1000.0; // Saldo inicial, puedes cambiarlo según tus necesidades
+
+        System.out.println("Bienvenido al Cajero Automático");
+        System.out.println("1. Consultar Saldo");
+        System.out.println("2. Depositar Dinero");
+        System.out.println("3. Retirar Dinero");
+
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Su saldo actual es: $" + saldo);
+                break;
+
+            case 2:
+                System.out.print("Ingrese la cantidad a depositar: $");
+                double deposito = scanner.nextDouble();
+                saldo += deposito;
+                System.out.println("Nuevo saldo: $" + saldo);
+                break;
+
+            case 3:
+                System.out.print("Ingrese la cantidad a retirar: $");
+                double retiro = scanner.nextDouble();
+                if (retiro <= saldo) {
+                    saldo -= retiro;
+                    System.out.println("Retiro exitoso. Saldo restante: $" + saldo);
+                } else {
+                    System.out.println("Fondos insuficientes");
+                }
+                break;
+
+            default:
+                System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+                break;
         }
-        
-        scanner.close(); // Cerrar el scanner al final
+
+        scanner.close();
     }
 }
